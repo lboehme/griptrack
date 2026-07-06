cec79f2c06d55578393ecf32c1ebc73f58942a9c
lboehme <lboehme@physik.uni-bielefeld.de>
2026-07-06 12:02:03 +0200

    changed db url

--- backend/db.py
+++ backend/db.py
@@ -2,7 +2,7 @@ import os
 
 from sqlmodel import Session, create_engine
 
-DATABASE_URL = os.environ.get("GRIPTRACK_DATABASE_URL", "sqlite:///./griptrack.db")
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./griptrack.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
